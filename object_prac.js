ee923cd06cc7bd0d052bf8ae38b74e3e29992e6e
Owen <1534661514@qq.com>
2022-05-27 15:11:42 +0800

    finish charpter object basics

--- object_prac.js
+++ object_prac.js
@@ -1,11 +1,70 @@
-let user = new Object();
-user = {};
+"use strict"
 
-user = {
-    name: "John",
-    age: 30
-};
+// let user = new Object();
+// user = {};
+
+// user = {
+//     name: "John",
+//     age: 30
+// };
 
 
+// for (key in user) {
+//     alert(user[key]);
+// }
 
-alert(user.name);
+// alert(user.name);
+
+// let user = {};
+// user['name'] = "John";
+// alert(user.name);
+// user['surname'] = "Smith";
+// alert(user.surname);
+
+
+// user['name'] = 'Pete';
+// alert(user.name);
+// delete user.name;
+// alert(user.name);
+// function isEmpty(object) {
+//     for (let key in object) {
+//         return false;
+//     }
+//     return true;
+// }
+
+// let schedule = {};
+// alert(isEmpty(schedule));
+// schedule["8:30"] = "get up";
+// alert(isEmpty(schedule));
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+
+// // function calcSum(object) {
+// //     let sum = 0;
+// //     for (let employee in object) {
+// //         sum += object[employee]
+// //     }
+// //     if (sum >= 0) {
+// //         return sum;
+// //     } else {
+// //         return 0;
+// //     }
+
+// // }
+
+// // alert(calcSum(salaries))
+
+// function multiplyNumeric(obj) {
+//     for (let employee in obj) {
+//         obj[employee] *= 2;
+//     }
+// }
+
+// multiplyNumeric(salaries);
+// for (let key in salaries) {
+//     alert(salaries[key]);
+// }
