7f22d4ca573bf3ed55339ac1d742225f9d662167
Owen <1534661514@qq.com>
2022-05-27 16:51:29 +0800

    finish charpter this

--- object_prac.js
+++ object_prac.js
@@ -126,3 +126,22 @@
 // alert(calculator.sum());
 // alert(calculator.mul());
 
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+
+    down() {
+        this.step--;
+        return this;
+    },
+
+    showStep() {
+        alert(this.step);
+        return this;
+    }
+}
+
+ladder.up().up().down().showStep().down().showStep()
