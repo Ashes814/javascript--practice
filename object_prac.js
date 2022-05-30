7ce49e200b591ff8fc6d5c6b7e0c4b53487e5c6f
Owen <1534661514@qq.com>
2022-05-30 15:53:08 +0800

    finish filterRange problem

--- object_prac.js
+++ object_prac.js
@@ -441,3 +441,14 @@
 // }
 
 // alert(camelize('list-style-image'));
+
+function filterRange(arr, a, b) {
+    return arr.filter(item => (item >= a && item <= b))
+}
+
+
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+alert( filtered ); // 3,1（匹配值）
+alert( arr ); // 5,3,8,1（未修改）
