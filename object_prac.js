f882960229b05558407ec8eed0e8693592935a70
Owen <1534661514@qq.com>
2022-05-30 16:32:24 +0800

    finish copyed sort problem

--- object_prac.js
+++ object_prac.js
@@ -471,3 +471,11 @@
 
 
 // alert( arr );
+// function copySorted(arr) {
+//     let copyArr = arr.slice(0);
+//     return copyArr.sort()
+// }
+// let arr = ['HTML', 'JavaScript', 'CSS'];
+// let sorted = copySorted(arr);
+// alert(sorted);
+// alert(arr);
