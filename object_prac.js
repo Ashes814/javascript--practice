f18e0301a38661ac4874d41404a02cbf0602c881
Owen <1534661514@qq.com>
2022-05-31 16:43:52 +0800

    solve problem groupBYid

--- object_prac.js
+++ object_prac.js
@@ -594,20 +594,47 @@
 
 // alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
 
-function unique(arr) {
-    let newArr = []
-    for (let i of arr) {
-        if (newArr.includes(i)) {
-            continue;
-        } else {
-            newArr.push(i);
-        }
-    }
-    return newArr;
-  }
+// function unique(arr) {
+//     let newArr = []
+//     for (let i of arr) {
+//         if (newArr.includes(i)) {
+//             continue;
+//         } else {
+//             newArr.push(i);
+//         }
+//     }
+//     return newArr;
+//   }
   
-  let strings = ["Hare", "Krishna", "Hare", "Krishna",
-    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+//   let strings = ["Hare", "Krishna", "Hare", "Krishna",
+//     "Krishna", "Krishna", "Hare", "Hare", ":-O"
+//   ];
+  
+//   alert( unique(strings) ); // Hare, Krishna, :-O
+function groupById(users) {
+    return users.reduce(function (prev, item) {
+        prev[item.id] = item
+        return prev },
+
+    {})
+}
+
+
+
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
   ];
   
-  alert( unique(strings) ); // Hare, Krishna, :-O
+  let usersById = groupById(users);
+  alert(usersById.john.id);
+  /*
+  // 调用函数后，我们应该得到：
+  
+  usersById = {
+    john: {id: 'john', name: "John Smith", age: 20},
+    ann: {id: 'ann', name: "Ann Smith", age: 24},
+    pete: {id: 'pete', name: "Pete Peterson", age: 31},
+  }
+  */
