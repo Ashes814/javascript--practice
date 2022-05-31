0c013acd00fd8b0146a97cecaa635c3c53ade8a7
Owen <1534661514@qq.com>
2022-05-31 14:29:37 +0800

    solve problem map to object

--- object_prac.js
+++ object_prac.js
@@ -499,14 +499,36 @@
 
 // alert( calc.calculate("3 + 7") ); // 10
 
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
 
-let users = [ john, pete, mary ];
+// let users = [ john, pete, mary ];
 
-let names = users.map(function (item) {
-    return item.name;
-})
+// let names = users.map(function (item) {
+//     return item.name;
+// })
 
-alert( names ); // John, Pete, Mary
+// alert( names ); // John, Pete, Mary
+
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [ john, pete, mary ];
+
+// let usersMapped = users.map( item => ({
+//     fullName: item.name + item.surname,
+//     id: item.id
+// }));
+
+// /*
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+// */
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // John Smith
