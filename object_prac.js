ad0c50a8db30500c0b8363ff8b1ecac894de74e1
Owen <1534661514@qq.com>
2022-06-06 20:25:32 +0800

    function in if

--- object_prac.js
+++ object_prac.js
@@ -1166,3 +1166,36 @@
 
 // // call it
 // work(); // 会显示什么？
+
+// function makeCounter() {
+//   let count = 0;
+
+//   return function() {
+//     return count++;
+//   };
+// }
+
+// let counter = makeCounter();
+// let counter2 = makeCounter();
+
+// alert( counter() ); // 0
+// alert( counter() ); // 1
+
+// alert( counter2() ); // ?
+// alert( counter2() ); // ?
+function Counter() {
+  let count = 0;
+
+  this.up = function() {
+    return ++count;
+  };
+  this.down = function() {
+    return --count;
+  };
+}
+
+let counter = new Counter();
+
+alert( counter.up() ); // ?
+alert( counter.up() ); // ?
+alert( counter.down() ); // ?
