b6b3caf6c5dcc450abf6062a998f8fab7b26f64b
Owen <1534661514@qq.com>
2022-06-07 12:15:46 +0800

    old var

--- object_prac.js
+++ object_prac.js
@@ -1251,27 +1251,27 @@
 // alert( users[0].name )
 // users.sort(byField('age'));
 // alert( users[0].name )
-function makeArmy() {
-  let shooters = [];
+// function makeArmy() {
+//   let shooters = [];
 
-  let i = 0;
-  while (i < 10) {
-    let b = i;
-    let shooter = function() { // 创建一个 shooter 函数，
+//   let i = 0;
+//   while (i < 10) {
+//     let b = i;
+//     let shooter = function() { // 创建一个 shooter 函数，
     
-      alert( b ); // 应该显示其编号
-    };
-    shooters.push(shooter); // 将此 shooter 函数添加到数组中
-    i++;
-  }
+//       alert( b ); // 应该显示其编号
+//     };
+//     shooters.push(shooter); // 将此 shooter 函数添加到数组中
+//     i++;
+//   }
 
-  // ……返回 shooters 数组
-  return shooters;
-}
+//   // ……返回 shooters 数组
+//   return shooters;
+// }
 
-let army = makeArmy();
+// let army = makeArmy();
 
-// ……所有的 shooter 显示的都是 10，而不是它们的编号 0, 1, 2, 3...
-army[0](); // 编号为 0 的 shooter 显示的是 10
-army[1](); // 编号为 1 的 shooter 显示的是 10
-army[2](); // 10，其他的也是这样。
+// // ……所有的 shooter 显示的都是 10，而不是它们的编号 0, 1, 2, 3...
+// army[0](); // 编号为 0 的 shooter 显示的是 10
+// army[1](); // 编号为 1 的 shooter 显示的是 10
+// army[2](); // 10，其他的也是这样。
