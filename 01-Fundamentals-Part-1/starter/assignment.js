1f5867d866e16ea3aa7adec7221b6432d33a68ba
Owen <1534661514@qq.com>
2022-06-11 15:10:18 +0800

    complete task country name

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,4 @@
+let country = 'China';
+let continent = 'Asia'
+let population = 1400000000;
+console.log(`Country:${country}, Continent:${continent}, Population:${population}`)
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+// let js = `amazing`;
+
+// if (js === `amazing`) {
+//   alert(`JavaScript is FUN!`);
+// }
+
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(114514);
+
+let firstName = "Jonas";
+console.log(firstName)
+let name = 10086; // not use name as a varaible
+console.log(name)
+const PI = 3.1415926535897932384626;
