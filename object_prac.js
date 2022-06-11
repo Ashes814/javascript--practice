79c736b558854883bb80f92ef9e87d74d7e2bf97
Owen <1534661514@qq.com>
2022-06-11 16:26:52 +0800

    binding with arguments

--- object_prac.js
+++ object_prac.js
@@ -1464,12 +1464,16 @@ function askPassword(ok, fail) {
 let user = {
   name: 'John',
 
-  loginOk() {
-    alert(`${this.name} logged in`);
-  },
+  // loginOk() {
+  //   alert(`${this.name} logged in`);
+  // },
 
-  loginFail() {
-    alert(`${this.name} failed to log in`)
-  },
+  // loginFail() {
+  //   alert(`${this.name} failed to log in`)
+  // },
+
+  login(result) {
+    alert( this.name + (result ? ' logged in ' : ' failed to log in ') )
+  }
 }
-askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+askPassword(user.login.bind(user, true), user.login.bind(user, false));
