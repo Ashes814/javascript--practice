2e081e1d0fb1ebe83b3ef7dbfa8d22cc856e070e
Owen <1534661514@qq.com>
2022-06-12 19:30:17 +0800

    Prototype inheritance

--- jsinfo.js
+++ jsinfo.js
@@ -1557,3 +1557,27 @@
 // };
 
 // console.log(bed.glasses)
+let hamster = {
+    //stomach: [],
+  
+    eat(food) {
+      this.stomach.push(food);
+    }
+  };
+  
+  let speedy = {
+      stomach:[],
+    __proto__: hamster
+  };
+  
+  let lazy = {
+    stomach:[],
+    __proto__: hamster
+  };
+  
+  // This one found the food
+speedy.eat("apple");
+console.log( speedy.stomach ); // apple
+  
+  // This one also has it, why? fix please.
+console.log( lazy.stomach ); // apple；
