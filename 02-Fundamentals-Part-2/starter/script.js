01456c1ee8594c7f2eba75133106e5d287a9c20d
Owen <1534661514@qq.com>
2022-06-17 16:07:56 +0800

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,30 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-const interface = '23';
+// // const interface = '23';
 
+// function logger() {
+//     console.log('My name is Jonas');
+// }
+
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+//     return juice;
+// }
+
+// console.log(fruitProcessor(1,3));
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`
+}
+
+let describeChina = describeCountry('China', 1400, 'Beijing');
+let describeRussia = describeCountry('Russia', 20, 'Moscow');
+let describeJapan = describeCountry('Japan', 140, 'Tokyo');
+console.log(describeChina, describeRussia, describeJapan);
