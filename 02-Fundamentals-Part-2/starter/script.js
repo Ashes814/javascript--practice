7e7ca5a7af0464bdad1a8b9036741b349ba26909
Owen <1534661514@qq.com>
2022-06-18 20:13:51 +0800

    dot and bracket

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -114,4 +114,4 @@ const myCountry = {
 
 }
 
-console.log(myCountry)
+console.log(`'${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.'`)
