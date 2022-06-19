02a37e7de4d66aed9abb930fcbafe06affc5a394
Owen <1534661514@qq.com>
2022-06-19 13:56:40 +0800

    developer skills & editor set up

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+
+
+
+// const xxs = 223;
+// const xxg = 34;
+
+// if (1 > 0) console.log('1')
+
+
+// const calcAge = birthYear => 2037 - birthYear;
+
+// console.log(xxg);
+// alert()
+
+function printForecast(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`${arr[i]}C in ${i+1} days`);
+    }
+}
+
+printForecast([17, 21, 23])
