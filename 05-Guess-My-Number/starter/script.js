d9dfb17fe07524c62d9be9917587f107ed57b61b
Owen <1534661514@qq.com>
2022-06-28 19:32:34 +0800

    dom event

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,3 +4,12 @@
 
 // document.querySelector('.guess').value = 10;
 // console.log(document.querySelector('.guess').value)
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        document.querySelector('.message').textContent = '❌❌❌ Oh NO, your input is not a Number!'
+    } else {
+        console.log(guess);
+    }
+
+})
