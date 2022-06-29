363ae8f0c2c8abe44ebf1e59d30fab93fa5e59fe
Owen <1534661514@qq.com>
2022-06-29 15:13:58 +0800

    implement again button

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,15 +4,16 @@
 
 // document.querySelector('.guess').value = 10;
 // console.log(document.querySelector('.guess').value)
-const answerNumber = Math.trunc(Math.random() * 20) + 1;
+let answerNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = answerNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     if (!guess) {
         document.querySelector('.message').textContent = '❌❌❌ Oh NO, your input is not a Number or A 0!'
-    } else if (guess === answerNumber){
+    } else if (guess === answerNumber) {
+        document.querySelector('.number').textContent = answerNumber;
         document.querySelector('.message').textContent = 'Congratulations!!';
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem'
@@ -40,4 +41,15 @@ document.querySelector('.check').addEventListener('click', function () {
 
     }
 
+})
+
+document.querySelector('.again').addEventListener('click', function() {
+    answerNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    document.querySelector('.guess').value= '';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem'
 })
