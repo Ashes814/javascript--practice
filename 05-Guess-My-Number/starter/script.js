dacea90741f04a5b9c2ff9b99b9e5dba36ecb7de
Owen <1534661514@qq.com>
2022-06-29 14:02:56 +0800

    implement guess number logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,12 +4,36 @@
 
 // document.querySelector('.guess').value = 10;
 // console.log(document.querySelector('.guess').value)
+const answerNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = answerNumber;
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     if (!guess) {
-        document.querySelector('.message').textContent = '❌❌❌ Oh NO, your input is not a Number!'
-    } else {
-        console.log(guess);
+        document.querySelector('.message').textContent = '❌❌❌ Oh NO, your input is not a Number or A 0!'
+    } else if (guess === answerNumber){
+        document.querySelector('.message').textContent = 'Congratulations!!';
+    } else if (guess > answerNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '🏋🏻Too High!!';
+            score--;
+            document.querySelector('.score').textContent = score;           
+        } else {
+            document.querySelector('.message').textContent = "🚫 Lost the game you stupid!"; 
+            document.querySelector('.score').textContent = 0;
+        }
+
+    } else if (guess < answerNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too Low!!🏋🏻';
+            score--;
+            document.querySelector('.score').textContent = score;           
+        } else {
+            document.querySelector('.message').textContent = "🚫 Lost the game you stupid!"; 
+            document.querySelector('.score').textContent = 0;
+        }
+
     }
 
 })
