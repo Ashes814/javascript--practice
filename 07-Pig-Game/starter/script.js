7c60c0b681a3817d51d6c9a619e49360c6431fbf
Owen <1534661514@qq.com>
2022-07-01 13:56:30 +0800

    holding current score

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0Element = document.querySelector('.player--0')
+const player1Element = document.querySelector('.player--1');
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
@@ -8,6 +10,9 @@ const current1Element = document.getElementById('current--1');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+const scores = [0, 0];
+let activePlayer = 0;
 let currentScore = 0;
 
 score0Element.textContent = 0;
@@ -27,7 +32,16 @@ btnRoll.addEventListener('click', function () {
     if (dice !== 1) {
         // Add dice to current score
         currentScore += dice;
-        current0Element.textContent = currentScore // change later
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        // current0Element.textContent = currentScore // change later
+    } else {
+        // Switch to next Player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = (activePlayer === 0) ? 1 : 0;
+        currentScore = 0;
+        player0Element.classList.toggle('player--active');
+        player1Element.classList.toggle('player--active');
+        
     }
 })
 
