5bb547c631ec97282b8b3cfbe4cf9e2b129a12a8
Owen <1534661514@qq.com>
2022-07-09 22:32:54 +0800

    optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,32 +1,32 @@
 'use strict';
 
 // Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// // Data needed for first part of the section
-// // const restaurant = {
-// //   name: 'Classico Italiano',
-// //   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-// //   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-// //   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-// //   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-// //   openingHours: {
-// //     thu: {
-// //       open: 12,
-// //       close: 22,
-// //     },
-// //     fri: {
-// //       open: 11,
-// //       close: 23,
-// //     },
-// //     sat: {
-// //       open: 0, // Open 24 hours
-// //       close: 24,
-// //     },
-// //   },
-// // };
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
 // // // const [a, b] = [1, 2];
 // // // const { openingHours: {thu:c, fri: d} } =restaurant
 // // // console.log(c);
@@ -168,7 +168,8 @@
 // team1 > team2 && console.log('team2');
 // team1 < team2 && console.log('team1')
 
-let arr = ['1', '2', '3'];
-console.log(arr.entries())
+// let arr = ['1', '2', '3'];
+// console.log(arr.entries())
 
 // object literals
+console.log(restaurant.openingHours.thu?.open);
