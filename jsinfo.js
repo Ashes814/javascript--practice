833ea7440bc23a27d942850a015017c85b213e6f
Owen <1534661514@qq.com>
2022-07-22 21:48:44 +0800

    update jsinfo

--- jsinfo.js
+++ jsinfo.js
@@ -1557,27 +1557,27 @@
 // };
 
 // console.log(bed.glasses)
-let hamster = {
-    //stomach: [],
+// let hamster = {
+//     //stomach: [],
   
-    eat(food) {
-      this.stomach.push(food);
-    }
-  };
+//     eat(food) {
+//       this.stomach.push(food);
+//     }
+//   };
   
-  let speedy = {
-      stomach:[],
-    __proto__: hamster
-  };
+//   let speedy = {
+//       stomach:[],
+//     __proto__: hamster
+//   };
   
-  let lazy = {
-    stomach:[],
-    __proto__: hamster
-  };
+//   let lazy = {
+//     stomach:[],
+//     __proto__: hamster
+//   };
   
-  // This one found the food
-speedy.eat("apple");
-console.log( speedy.stomach ); // apple
+//   // This one found the food
+// speedy.eat("apple");
+// console.log( speedy.stomach ); // apple
   
-  // This one also has it, why? fix please.
-console.log( lazy.stomach ); // apple；
+//   // This one also has it, why? fix please.
+// console.log( lazy.stomach ); // apple；
