b6caaf4c050034a3d80e25b073a8e0f42ed552a4
Owen <1534661514@qq.com>
2022-07-28 14:12:02 +0800

    callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,2 +1,36 @@
 'use strict';
-/
+const flight = 'LH234';
+
+const zow = {
+    name: '欧宝多看书',
+    passport: 13167117098
+};
+
+// const checkIn = function (flightNum, passenger) {
+    
+// }
+
+// checkIn(flight, jonas);
+
+
+// first class and higher order functions
+
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+const transformer = function (str, fn) {
+    console.log(`Transformed string ${fn(str)}`)
+}
+
+transformer('JavaScript is the best', upperFirstWord);
+
+const high5 = function () {
+    console.log('tietie');
+}
+['Jonas', 'sdf', 'sdf'].forEach(high5)
