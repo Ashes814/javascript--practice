03ef87e9efe466adcf8b433f56c2f9f80ec7a8a6
张欧文 <zowcool@zhangouwendeMacBook-Pro.local>
2022-09-19 16:32:26 +0800

    implementing smooth and scrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,17 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(e) {
+  // const s1coords = section1.getBoundingClientRect()
+  
+  // window.scrollTo(s1coords.left, s1coords.top);
+
+  section1.scrollIntoView({behavior: 'smooth'});
+})
+
 
 const openModal = function (event) {
   event.preventDefault();
