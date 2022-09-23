169fb705581a28bcf9b22de61e2c0f57a96f29ca
张欧文 <zowcool@zhangouwendeMacBook-Pro.local>
2022-09-23 17:34:39 +0800

    buiding a tabbed component

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -69,13 +69,43 @@ message.style.width = '120%';
 //   })
 // })
 
-document.querySelector('nav__links').addEventListener('click', function(e) {
+document.querySelector('.nav__links').addEventListener('click', function(e) {
   
 
   if (e.target.classList.contains('nav__link')) {
     e.preventDefault();
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
   }
 })
+
+
+// Tabbed components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e) {
+
+
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  tabs.forEach(function(t) {
+    t.classList.remove('operations__tab--active');
+  })
+  tabsContent.forEach(function(t) {
+    t.classList.remove('operations__content--active');
+  })
+  clicked.classList.add('operations__tab--active');
+
+  //active content
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+})
+
+
+
 // random color
 // const randomInt = function(min, max) {
 //   return Math.floor(Math.random() * (max - min + 1) + min)
