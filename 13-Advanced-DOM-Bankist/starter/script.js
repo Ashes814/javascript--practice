53d1a5dccaf51f6f99cdf1b873271008bc8b19aa
张欧文 <zowcool@zhangouwendeMacBook-Pro.local>
2022-09-26 16:36:11 +0800

    Lazy Loading Images

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -175,9 +175,10 @@ const revealSection = function(entries, observer) {
   const [entry] = entries;
 
   if (!entry.isIntersecting) {
-    return
+    return;
   }
   entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
 }
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -189,6 +190,33 @@ allSections.forEach(function(section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 })
+
+
+// Lazy load image
+const loadImg = function(entries, observe) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    return;
+  };
+
+  entry.target.src = entry.target.dataset.src;
+  
+
+  entry.target.addEventListener('load', function() {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+
+}
+const imgTargets = document.querySelectorAll('img[data-src]');
+const imgObserver = new IntersectionObserver(loadImg, {
+  root:null,
+  threshold:0,
+  rootMargin: '-200px',
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
 // random color
 // const randomInt = function(min, max) {
 //   return Math.floor(Math.random() * (max - min + 1) + min)
