0b37f45209a7a998a78553694cf3966f83188a88
张欧文 <zowcool@zhangouwendeMacBook-Pro.local>
2022-09-26 15:53:14 +0800

    Revaling element on scrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -166,6 +166,29 @@ const obsOptions = {
 
 const observer = new IntersectionObserver(obsCallbacks, obsOptions);
 observer.observe(header);
+
+
+// remove hidden
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    return
+  }
+  entry.target.classList.remove('section--hidden');
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
 // random color
 // const randomInt = function(min, max) {
 //   return Math.floor(Math.random() * (max - min + 1) + min)
