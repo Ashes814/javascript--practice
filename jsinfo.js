4b366a9924c2be542b7d982313dd944b912339e4
张欧文 <zowcool@zhangouwendeMacBook-Pro.local>
2022-10-04 10:21:56 +0800

    finish F.prototype

--- jsinfo.js
+++ jsinfo.js
@@ -1623,3 +1623,63 @@
 // for (let prop in ob) {
 //     ob.hasOwnProperty(prop) ? console.log(prop) : console.log('nonono');
 // };
+
+let animal = {
+    jumps: null
+  };
+  let rabbit = {
+    __proto__: animal,
+    jumps: true
+  };
+  
+// alert( rabbit.jumps ); // true (1)
+
+// delete rabbit.jumps;
+
+// alert( rabbit.jumps ); // null (2)
+
+// delete animal.jumps;
+
+// alert( rabbit.jumps ); // not defined (3)
+// let head = {
+//     glasses: 1
+//   };
+  
+//   let table = {
+//     pen: 3,
+//     __proto__: head
+//   };
+  
+//   let bed = {
+//     sheet: 1,
+//     pillow: 2,
+//     __proto__: table
+//   };
+  
+//   let pockets = {
+//     money: 2000,
+//     __proto__: bed
+//   };
+let hamster = {
+    stomach: [],
+  
+    eat(food) {
+      this.stomach = [];
+      this.stomach.push(food);
+    }
+  };
+  
+  let speedy = {
+    __proto__: hamster
+  };
+  
+  let lazy = {
+    __proto__: hamster
+  };
+  
+  // 这只仓鼠找到了食物
+speedy.eat("apple");
+alert( speedy.stomach ); // apple
+
+// 这只仓鼠也找到了食物，为什么？请修复它。
+alert( lazy.stomach ); // apple
