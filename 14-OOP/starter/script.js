fef7e6cd152b3a5c576d5d64059a9da71d534503
张欧文 <zowcool@zhangouwendeMacBook-Pro.local>
2022-10-09 16:11:42 +0800

    Finish code challenage1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,49 @@
 'use strict';
+
+// const Person = function(name, birth) {
+//     this.name = name;
+//     this.birth = birth;
+// }
+
+// Person.prototype.calcAge = function() {
+//     console.log(2037 - this.birth);
+// }
+// let oo = new Person('oo', 1999);
+// console.log(oo);
+// oo.calcAge()
+
+
+// Array.prototype.uuuukkk = function() {
+//     return [...new Set(this)];
+// }
+
+// console.log([1,1,1,2,2,2,3,3,3].uuuukkk());
+// // Array.prototype = {};
+
+// // [11,22].fill(value > 8);
+
+// console.dir(document.querySelector('h1'))
+//challenage 1
+const Car = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(this.speed);
+}
+
+Car.prototype.brake = function() {
+    this.speed -= 5;
+    console.log(this.speed);
+}
+
+let bmw = new Car('BMW', 120);
+let ferrari = new Car('Ferrari', 320);
+
+
+bmw.brake();
+bmw.brake();
+
+ferrari.accelerate();
