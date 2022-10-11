b325dd7e5edb421bd702361bf671ca0611f281ff
张欧文 <zowcool@zhangouwendeMacBook-Pro.local>
2022-10-11 13:13:00 +0800

    object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -20,65 +20,65 @@
 // console.log([1,1,1,2,2,2,3,3,3].uuuukkk());
 // // Array.prototype = {};
 
-// // [11,22].fill(value > 8);
+// // // [11,22].fill(value > 8);
 
-// console.dir(document.querySelector('h1'))
-//challenage 1
-// const Car = function(make, speed) {
-//     this.make = make;
-//     this.speed = speed;
-// };
+// // console.dir(document.querySelector('h1'))
+// //challenage 1
+// // const Car = function(make, speed) {
+// //     this.make = make;
+// //     this.speed = speed;
+// // };
 
-// Car.prototype.accelerate = function() {
-//     this.speed += 10;
-//     console.log(this.make + this.speed);
+// // Car.prototype.accelerate = function() {
+// //     this.speed += 10;
+// //     console.log(this.make + this.speed);
+// // }
+
+// // Car.prototype.brake = function() {
+// //     this.speed -= 5;
+// //     console.log(this.make + this.speed);
+// // }
+
+// // let bmw = new Car('BMW', 120);
+// // let ferrari = new Car('Ferrari', 320);
+
+
+// // bmw.brake();
+// // bmw.brake();
+
+// // ferrari.accelerate();
+// class Person {
+//     constructor(name, birth) {
+//         this.name = name;
+//         this.birth = birth;
+//     }
+
+//     calcAge() {
+//         console.log(2037 - this.birth);
+//     }
+
+//     set name(name) {
+//         if (name.includes(' ')) {
+//             this._name = name;
+//         } else {
+//             alert('Name ERROR')
+//         }
+//     }
+
+//     get name() {
+//         return this._name;
+//     }
+
+//     static stHey() {
+//         console.log('Hi')
+//     }
 // }
 
-// Car.prototype.brake = function() {
-//     this.speed -= 5;
-//     console.log(this.make + this.speed);
-// }
-
-// let bmw = new Car('BMW', 120);
-// let ferrari = new Car('Ferrari', 320);
-
-
-// bmw.brake();
-// bmw.brake();
-
-// ferrari.accelerate();
-class Person {
-    constructor(name, birth) {
-        this.name = name;
-        this.birth = birth;
-    }
-
-    calcAge() {
-        console.log(2037 - this.birth);
-    }
-
-    set name(name) {
-        if (name.includes(' ')) {
-            this._name = name;
-        } else {
-            alert('Name ERROR')
-        }
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    static stHey() {
-        console.log('Hi')
-    }
-}
-
-let testP = new Person('fff f', 1999);
-console.log(testP);
-console.log(testP.name);
-Person.stHey()
-testP.stHey()
+// let testP = new Person('fff f', 1999);
+// console.log(testP);
+// console.log(testP.name);
+// Person.stHey()
+// testP.stHey()
 // const account = {
 //     name: 'oo',
 //     movements: [1,2,3,4],
@@ -89,4 +89,13 @@ testP.stHey()
 // }
 
 // console.log(account.latest);
+
+const personProto = {
+    calcAge() {
+        return 2037 - this.birthyear;
+    }
+};
+
+let test = Object.create(personProto);
+console.log(test);
 
