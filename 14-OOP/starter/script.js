6e9e4f988eaeb6bc96897eea5fefb5a08146fd6e
张欧文 <zowcool@zhangouwendeMacBook-Pro.local>
2022-10-11 16:12:38 +0800

    chaining methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -114,36 +114,121 @@
 // //     console.log(this.make + this.speed);
 // // }
 
-class CarCl {
-    constructor(make, speed) {
-        this.make = make;
-        this.speed = speed;
+// class CarCl {
+//     constructor(make, speed) {
+//         this.make = make;
+//         this.speed = speed;
+//     }
+
+//     accelerate() {
+//         this.speed += 10;
+//         console.log(this.make + this.speed);
+//     }
+
+//     brake() {
+//         this.speed -= 5;
+//         console.log(this.make + this.speed);
+//     }
+
+//     get speedUS() {
+//         return this.speed / 1.6
+//     }
+
+//     set speedUS(milespeed) {
+//         this.speed = milespeed * 1.6
+//     }
+// }
+
+// let car1 = new CarCl('Ford', 120);
+// console.log(car1);
+// car1.accelerate();
+// car1.brake();
+
+// console.log(car1.speedUS)
+// car1.speedUS = 60;
+// console.log(car1.speedUS,car1.speed);
+
+// const Person = function(name, birth) {
+//     this.name = name;
+//     this.birth = birth;
+// }
+
+// const Student = function(name, birth, course) {
+//     Person.call(this, name, birth);
+//     this.course = course;
+// // }
+
+// // Student.prototype = Object.create(Person.prototype);
+// // Student.prototype.introduce = function() {
+// //     console.log(`My name is ${this.name}, I was born in ${this.birth}, and I study at ${this.course}`);
+// // }
+
+// // const mike = new Student('Mike', 1999, 'GIS');
+
+// // mike.introduce();
+
+// const Car = function(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function() {
+//     this.speed += 10;
+//     console.log(this.make + this.speed);
+// }
+
+// Car.prototype.brake = function() {
+//     this.speed -= 5;
+//     console.log(this.make + this.speed);
+// }
+
+// const EV = function(make, speed, charge) {
+//     Car.call(this, make, speed);
+//     this.charge = charge;
+// };
+
+// EV.prototype = Object.create(Car.prototype);
+// EV.prototype.chargeBattery = function(chargeTo) {
+//     this.charge = chargeTo;
+// };
+
+// EV.prototype.accelerate = function() {
+//     this.speed += 20;
+//     this.charge -= 1;
+//     console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`)
+// };
+
+// let car1 = new EV('Tesla', 120, 23);
+// console.log(car1);
+// car1.brake();
+// car1.accelerate();
+
+class Account {
+    // public field
+    locale = navigator.language;
+    #movements = [];
+    #pin;
+
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this.#pin = pin;
+        // this.movements = [];
+        // this.locale = navigator.language;
+
     }
 
-    accelerate() {
-        this.speed += 10;
-        console.log(this.make + this.speed);
+    get movements() {
+        return this.#movements;
     }
 
-    brake() {
-        this.speed -= 5;
-        console.log(this.make + this.speed);
-    }
-
-    get speedUS() {
-        return this.speed / 1.6
-    }
-
-    set speedUS(milespeed) {
-        this.speed = milespeed * 1.6
+    deposit(value) {
+        this.#movements.push(value);
+        return this;
     }
 }
 
-let car1 = new CarCl('Ford', 120);
-console.log(car1);
-car1.accelerate();
-car1.brake();
-
-console.log(car1.speedUS)
-car1.speedUS = 60;
-console.log(car1.speedUS,car1.speed);
+const acc1 = new Account('oo', 'RMB', 8848)
+acc1.deposit(848).deposit(8848);
+console.log(acc1.movements);
+// console.log(acc1.#movements);
