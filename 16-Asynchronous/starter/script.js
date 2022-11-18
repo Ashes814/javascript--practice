45b00858a6dbe87283e765f7efa1546d81ac72b6
张欧文 <1534661514@qq.com>
2022-11-18 09:55:47 +0800

    buidling chaining promise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -62,14 +62,19 @@ const getCountryAndNeighbour = function (country) {
 
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(function (response) {
-      // console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+    .then(response => response.json())
+    .then(data => {
       renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0], 'neighbour');
     });
 };
 
-getCountryData('portugal');
+getCountryData('korea');
