7ae0f30320f43126d132b7f7596ac52b4bcabf83
张欧文 <1534661514@qq.com>
2022-11-20 15:57:40 +0800

    building a promise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -170,13 +170,40 @@ const countriesContainer = document.querySelector('.countries');
 // btn.addEventListener('click', function () {
 //   getCountryData('JAPAN');
 // });
-console.log('Test start');
-setTimeout(() => console.log(`0 sec timer`), 0);
-Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// console.log('Test start');
+// setTimeout(() => console.log(`0 sec timer`), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
 
-for (let i = 0; i < 12000000000; i++) {}
-console.log('Test END');
+// for (let i = 0; i < 12000000000; i++) {}
+// console.log('Test END');
 
 // const lotteryPromise = new Promise(function(resolve, reject) {
 //     if(Math.random)
 // })
+
+// const lotterPromise = new Promise(function (resolve, reject) {
+//   console.log('Lotter draw is happening');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You WIN !');
+//     } else {
+//       reject(new Error('You lose !'));
+//     }
+//   }, 2000);
+// });
+
+// lotterPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('2 s pass');
+    return wait(1);
+  })
+  .then(() => console.log('3 s passs'));
