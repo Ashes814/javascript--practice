c03490dc7c4a2649036668e625b442f9df537f39
张欧文 <1534661514@qq.com>
2022-11-20 16:29:44 +0800

    promisify geolocation api

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -6,57 +6,29 @@ const testData3 = [-33.933, 18.474];
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-// const getJSON = function (url, errorMsg = 'Something went wrong') {
-//   return fetch(url).then(response => {
-//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
-//     return response.json();
-//   });
-// };
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
 
-// const renderCountry = function (data, className = '') {
-//   const html = `<article class="country ${className}">
-//       <img class="country__img" src="${data.flags.svg}" />
-//       <div class="country__data">
-//         <h3 class="country__name">${data.name.common}</h3>
-//         <h4 class="country__region">${data.region}</h4>
-//         <p class="country__row"><span>👫</span>${(
-//           +data.population / 1000000
-//         ).toFixed(1)} people</p>
-//         <p class="country__row"><span>🗣️</span>${data.fifa}</p>
-//         <p class="country__row"><span>💰</span>${data.subregion}</p>
-//       </div>
-//     </article>`;
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+      <img class="country__img" src="${data.flags.svg}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.fifa}</p>
+        <p class="country__row"><span>💰</span>${data.subregion}</p>
+      </div>
+    </article>`;
 
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-// };
-
-// const whereAmI = function (coordinate) {
-//   const lat = String(coordinate[0]);
-//   const lng = String(coordinate[1]);
-//   const geocodingAPI = '498707668562872975323x55949';
-//   const url = `https://geocode.xyz/${lat},${lng}?geoit=json&auth=${geocodingAPI}`;
-//   fetch(url)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(`Too Fast ${response.status}`);
-//       }
-
-//       return response.json();
-//     })
-//     .then(data => {
-//       console.log(data);
-//       console.log(`You are in ${data.city}, ${data.country}`);
-//       return data;
-//     })
-//     .then(function (data) {
-//       const countrAb = data.prov;
-//       getJSON(`https://restcountries.com/v3.1/alpha/${countrAb}`).then(data => {
-//         console.log(data);
-//         renderCountry(data[0]);
-//       });
-//     })
-//     .catch(err => console.log(err));
-// };
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
 
 // whereAmI(testData1);
 // whereAmI(testData2);
@@ -195,15 +167,72 @@ const countriesContainer = document.querySelector('.countries');
 // lotterPromise.then(res => console.log(res)).catch(err => console.error(err));
 
 // Promisifying setTimeout
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(2)
+//   .then(() => {
+//     console.log('2 s pass');
+//     return wait(1);
+//   })
+//   .then(() => console.log('3 s passs'));
+
+// console.log('Getting positon');
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
 
-wait(2)
-  .then(() => {
-    console.log('2 s pass');
-    return wait(1);
-  })
-  .then(() => console.log('3 s passs'));
+// getPosition().then(
+//   pos => console.log(pos),
+//   err => console.log(err)
+// );
+
+const whereAmI = function () {
+  //     const lat;
+  //   const lng;
+  getPosition()
+    .then(pos => {
+      console.log(pos.coords.latitude);
+      const { latitude: lat, longitude: lng } = pos.coords;
+      const geocodingAPI = '498707668562872975323x55949';
+      const url = `https://geocode.xyz/${lat},${lng}?geoit=json&auth=${geocodingAPI}`;
+      return fetch(url);
+    })
+
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Too Fast ${response.status}`);
+      }
+
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+      return data;
+    })
+    .then(function (data) {
+      const countrAb = data.prov;
+      getJSON(`https://restcountries.com/v3.1/alpha/${countrAb}`).then(data => {
+        console.log(data);
+        renderCountry(data[0]);
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+// whereAmI([59.508, 13.381]);
+// getPosition().then(pos => {
+//   console.log(pos.coords.latitude);
+// });
+whereAmI();
