592e4b90da981e836bc1ce0b5a951f6f2f645a98
张欧文 <1534661514@qq.com>
2022-11-30 14:47:28 +0800

    top level await

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -2,6 +2,21 @@
 // import { addToCart, shippingCost } from './shoppingCart.js';
 import * as ShoppingCart from './shoppingCart.js';
 // import {  } from './shoppingCart.js';
-console.log('Importing Module');
-console.log(ShoppingCart.shippingCost);
-ShoppingCart.addToCart('nima', 20);
+// console.log('Importing Module');
+// console.log(ShoppingCart.shippingCost);
+// ShoppingCart.addToCart('nima', 20);
+
+// const result = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+// const data = await result.json();
+// console.log(data);
+
+const getLastPost = async function () {
+  const result = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await result.json();
+  console.log(data);
+
+  //   return { id: data.at(-1) };
+};
+
+getLastPost();
+console.log('something');
