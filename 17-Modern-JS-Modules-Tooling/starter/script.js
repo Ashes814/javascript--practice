52b94ed097f34baac4c985d1d25be657b1c9f086
张欧文 <1534661514@qq.com>
2022-11-30 14:57:51 +0800

    the module pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -10,13 +10,38 @@ import * as ShoppingCart from './shoppingCart.js';
 // const data = await result.json();
 // console.log(data);
 
-const getLastPost = async function () {
-  const result = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  const data = await result.json();
-  console.log(data);
+// const getLastPost = async function () {
+//   const result = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//   const data = await result.json();
+//   console.log(data);
 
-  //   return { id: data.at(-1) };
-};
+//   //   return { id: data.at(-1) };
+// };
 
-getLastPost();
-console.log('something');
+// getLastPost();
+// console.log('something');
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apple', 4);
